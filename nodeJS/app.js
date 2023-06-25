8aa202d43b88e1844129d39773f16c587f36ea42
Christian <chris@sectorfive.com>
2023-06-25 19:23:01 -0400

    updating apis

--- nodeJS/app.js
+++ nodeJS/app.js
@@ -1,11 +1,12 @@
 const createError = require('http-errors'),
-      express = require('express'),
-      path = require('path'),
-      cookieParser = require('cookie-parser'),
-      logger = require('morgan');
+	express = require('express'),
+	path = require('path'),
+	cookieParser = require('cookie-parser'),
+	logger = require('morgan'),
+	cors = require('cors');
 
 const indexRouter = require('./routes/index'),
-      apiRouter = require('./routes/api');
+	apiRouter = require('./routes/api');
 
 const app = express();
 
@@ -13,6 +14,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -24,18 +26,18 @@ app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  next(createError(404));
+	next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
