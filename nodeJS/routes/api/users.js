c53bce61c55468c56441e20911dcb3b78d9dc9f3
Christian Knoll <chris@sectorfive.com>
2023-07-16 12:20:18 -0400

    various length user names and emails

--- nodeJS/routes/api/users.js
+++ nodeJS/routes/api/users.js
@@ -2,9 +2,9 @@ const express = require('express'),
 	router = express.Router();
 
 const users = [
-	{ id: 123, name: 'John Doe1', email: 'jdoe1@example.com' },
-	{ id: 456, name: 'John Doe2', email: 'jdoe2@example.com' },
-	{ id: 789, name: 'John Doe3', email: 'jdoe3@example.com' },
+	{ id: 123, name: 'Charles Francis Xavier', email: 'professor.x@example.com' },
+	{ id: 456, name: 'Scott Summers', email: 'cyclops.com' },
+	{ id: 789, name: 'Robert Louis Drake', email: 'iceman@example.com' },
 ];
 
 router.get('/', (req, res) => {
