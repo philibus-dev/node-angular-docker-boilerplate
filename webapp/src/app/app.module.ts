ab3bc063f20458fe6347b64f60ef9cb98308f7b7
Philip Montgomery <wfsdesign78@gmail.com>
2023-08-09 19:30:52 -0400

    revert back to original auth0 values.

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -19,18 +19,9 @@ import { AuthModule } from '@auth0/auth0-angular';
     HttpClientModule,
     ReactiveFormsModule,
     AppHeaderModule,
-    /*
     AuthModule.forRoot({
       domain: 'dev-7rkghlo8iznv4sgu.us.auth0.com',
       clientId: '64xH2OlGPcRp23ot9NXcXfCVERDsAbtH',
-      authorizationParams: {
-        redirect_uri: window.location.origin,
-      },
-    }),
-    */
-    AuthModule.forRoot({
-      domain: 'dev-qkgdrbdgyyde0h6e.us.auth0.com',
-      clientId: 'T8tBEz4DHjMsIXbzpJHlvPz9ciBgNVnL',
       authorizationParams: {
         redirect_uri: window.location.origin,
       },
