433e0d70cb85270fbb96306671da2334ba0d7304
Christian <chris@sectorfive.com>
2023-08-09 18:07:51 -0400

    auth setup

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -5,12 +5,29 @@ import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { AppHeaderModule } from "./components/app-header/app-header.module";
+import { AppHeaderModule } from './components/app-header/app-header.module';
+
+// Import the Auth0 module from the SDK
+import { AuthModule } from '@auth0/auth0-angular';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, HttpClientModule, ReactiveFormsModule, AppHeaderModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    AppHeaderModule,
+    AuthModule.forRoot({
+      domain: 'dev-7rkghlo8iznv4sgu.us.auth0.com',
+      clientId: '64xH2OlGPcRp23ot9NXcXfCVERDsAbtH',
+      authorizationParams: {
+        redirect_uri: window.location.origin,
+      },
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
