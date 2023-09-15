333c876bf7ec7721b203f1c44c4cb6ef7a0402e5
philibus-dev <wfsdesign78@gmail.com>
2023-09-15 16:37:51 -0400

    Update users.service.spec.ts

--- webapp/src/app/services/users.service.spec.ts
+++ webapp/src/app/services/users.service.spec.ts
@@ -3,14 +3,37 @@ import { TestBed } from '@angular/core/testing';
 import { UsersService } from './users.service';
 
 describe('UsersService', () => {
-  let service: UsersService;
+  let service: UsersService,
+    httpControler: HttpTestingController;
+
+  const mockUsers: User[] = [
+    {
+      userId: 'abc123'
+    }
+  ];
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(UsersService);
+    httpControler = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should get all users when getAllUsers is called', fakeAsync(() => {
+
+    service.getAllUsers().subscribe((res) => {
+      expect(res).toEqual(mockUsers);
+    });
+
+    const req = httpControler.expectOne({
+      method: 'GET',
+      url: '/api/users'
+    });
+
+    req.flush(mockUsers);
+
+  }));
 });
