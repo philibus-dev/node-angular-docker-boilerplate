94dbb5617f57298609fd16458e50457670f47948
philibus-dev <wfsdesign78@gmail.com>
2023-09-15 16:41:35 -0400

    Update user-list.component.spec.ts

--- webapp/src/app/containers/user-list/user-list.component.spec.ts
+++ webapp/src/app/containers/user-list/user-list.component.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed } from '@angular/core/testing';
 import { UserListComponent } from './user-list.component';
 
+let getAllUsersSpy: Spy;
+
 describe('UserListComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -10,10 +12,18 @@ describe('UserListComponent', () => {
     }).compileComponents();
   });
 
+  beforeEach(() => {
+    getAllUsersSpy = spyOn(component, 'getAllUsers').and.callThrough();
+  });
+
   it('should create the app', () => {
     const fixture = TestBed.createComponent(UserListComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
+  });
+
+  it('should call all users on load', () => {
+    expect(getAllUsersSpy).toHaveBeenCalled();
   });
 
 });
